8090b2241da622f4b06849c595666ed28a1a6bb9
Parag Garg <36436348+gitmrac@users.noreply.github.com>
2018-03-03 14:45:22 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,3 +2,10 @@ console.log('Loaded!');
 //change of text
 var element=document.getElementById('main-text');
 element.innerHTML='New Value';
+//move the image
+
+
+var img = document.getElementById('img');
+img.onclick=function(){
+    img.style.marginLeft='100px';
+};
