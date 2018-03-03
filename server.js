2a37deddcf86f0c43977803433c34300d51e1d2f
Parag Garg <36436348+gitmrac@users.noreply.github.com>
2018-03-03 13:00:26 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -69,13 +69,13 @@ function createTemplate(data){
 }
 
 app.get('/', function (req, res) {
-  res.send(createTemplate(articleOne));
+  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
 app.get('/:articleName',function(req, res){
     //articleName==article-one
     var articleName = req.params.articleName;
-  res.send(articles[articleName]);
+  res.send(createTemplate(articles[articleName]));
 });
 
 app.get('/ui/style.css', function (req, res) {
